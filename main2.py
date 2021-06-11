a074a548767f57e8e1857fff7be3b58655a88671
merlin7 <40037286+merlin93289528@users.noreply.github.com>
2021-06-12 02:00:57 +0300

    V1.0

---
+++ main2.py
@@ -0,0 +1,86 @@
+def max_tuple(tuples):
+    max_len = 0
+    tid = 0
+    for tup in tuples:
+        if len(tup) > max_len:
+            max_len = len(tup)
+            tid = tuples[tuples.index(tup)]
+
+    return tid
+
+text1 = "YkpaiHa, Ykperi , M. vis, Bn, MoCKOBC Ten: +38(044) 498- Bupo6HMK: Johrs son S.p.A., Va Ardeatina Santa Palomba, 006: ezia, Rome, ltaly. <lbxOHCOH C.n.A, Bia Apneariet 0, CaHTa lanoM6a, 0007 PHM, rani. LPM-2 O5'CM: 400 M RUD CpOK ropHOCTH 3 rona c naTbi H3roTOBNneHWA, yKa3aHHO Ha KKapaMAbinbiK MepaiMi: KanraMana KepceTinreH eHAipinr 6aCTan 3 bin. JSBF Campus de Maigremont 27100 Val de Reuil-FRANCE Registered trade mark. Made in Italy 3090P 04:03 PROD 11/2020 COCTAB/KYPAMb INGREDIENTS Aqua, Sodium Laureth Sulfate, Glycerin, Barbadensis Leaf Juice, Pyrus Malus Flower Extract, Polyquaternium-7, Citric Acid, Sodium Hydroxide, Potassium Sorbate, Sodium Benzoate, Sodium Sulfite, Parfum. [PR-018629] Cocamidopropyl Sodium Betaine, Aloe Chloride, 717040 EAL 12 M 06bem/06'em/Menuiepi: 400 M/mle" 
+text2 = "Anpendung Das Körperspray aus ca. 15 Cm Abstand auf den Körper sprühen Nicht in die Augen sprunen ents Aqua, Alcohot Denat, PEG-40 oro ated Castor Oil, Parfum, Panthenod apaya Extract, Mangifera IndiCa o alycerin, Urea, Lactic A Glycine, Proline, Benzopre One-4, Citric Acid, So0 uhloride, Sodiumou Linalool Lno Citra ellol GeraniO 47005, CI 15510 Bestandteile des Parfumols ROSSMANN Qualitätsmar stellt in Deutschlan 19885421"
+text3 = "8809500811053 :1) &P A8 AIS RHEA87 EHA PSEAE E 132 12 [EE001 398HIAI88| 2AI'A8 19E 18,3848 (49) 88 530 ml Au|TSSN 02 ) 904-7079 INGREDIENTS: Water, Cocamidopropyl Betaine, Disodium Laureth Suifosucinate, larae Oide, TEA-Cocoyl Glutamate, Glycerin, Butylene Glycol, Lauramide DEA, PEG-120 Mehy âi Doeate, Sodium Benzoate, PEG-7 Glyceryl Cocoate, PPG-3 Caprylyl Ether, Ctric Ait Pi nu-0, Betaine, Hydrawypropyl Chitosan, Hydrolyzed Wheat Protein, Hydroye a Tetalke Hjdolyaed Soy Protein, Hydrolyzed Silk, Hydroxyacetophenone, Sodium Hyaluronate Dipropyene Glycol, Zein, Argania Spinosa Kernel Oil, Persea Gratissima (Avocadol Ol G DIRECTIONS: Massage shampoo throughout wet halr and d Rinse scalp. thorous ontnue to ad watr hNate a rich lather from the highly concentrated formula. Rinse thoroughly CAUTIONS IN USE 1. In case of having side effects such as red rash, swole USing this cosmetic, or in case such sympto ms appear by part direct wne sun dermatitis. 3. Precautions for treatmentA storage. ln case 1Nec of ss th of a .Rinse well with water. if not, it could lead to hair 0na seed Ol, Simmondsia Chinensis (Uojoba) Seed Oil, Fragral Repeat if needed. ght afterus tching while y t, consult a dermatold e jou taease thior denmatithic mediately. 2. Do not apply it on the part wof th idren. 2JAboid the p mmedu place where the direct anufactured by.o ufactured for JPPROFESSIONAL Co.,LTD. .newgencosmet TT-/ ro, Gangbuk-gu, Seoul, Republic of Korea HTTP:| www.LADOR.CO. MADE IN KOREA DPE ERI"
+text4 = """NPMMEHEHME YrpoM H BeMepoM, CHOVHB BATHBIM AMCK. HaHecHTe Ha nHYO MOBTOpMTe po Tex M wero. He TpeőyeT TpeHAA nop noka BaTHBIM AMCK He CraHer MneanbHo 4HCTBIM. M CMbIBaHMA. 6E3 CNMPTA. BE3 OTAYLWEK ipOTecTwpoBAHO NOA AEpMaToNornyecKUM M OdDTONBManormyeckUM KOHTpOneM. LEHHOCTH L'OREAL PARIS Be3onacHocTb M 3hheKTMBHOCTb AOKa3aHa He3aBW CMMBIMM MHCTHTyTaMM npM MMHMMyM 600 HaHeceHMAX Ha KOXy, BKNKO4aA uyBCTBMTeibHylo. LOréal npwaepxeH MeTOgaM TeCTHPOBaHHA 6e3 KMBOTHBIX 6onee 30 neT. 781988 1-COCTABICKNAD: AQUA /WATER HEXYLENE GLYCOL GLYCERIN .POLOXAMER 184 DISODIUM COCOAMPHODIACETAIE DISODIUM EDTA POLYAMINOPROPYL BIGUANIDE. (FIL.L. B548942 FOEAU IOpeare), 119180, . Mooxsa, 4HTanyreHwHcoAÁ nep, 18, Cīp.1-2. Ten: (454913 EHE. TEaTb Iporyayion poinuaNg| TMBX 34p Ko. KT Hertzstrasse 175 DI6187 KARLSRURE, GEmay epk3ARL IQgeH pO: CHL H2 ynaoere. TenepOH TopAe HEHM NOpeanb 1lapex8 800 10 9 0. b CTHpTa-He CopepoAT 3THTIOBAĂ CTHDT. "YnonHoMOHEeHHBN nOepCraBHTens k3roT08MTenol HMNODTeP. 28T602 06/23 3MHBaHR BOONO. 33cnoxowe Wipy. HawociTH 8a Wpy Donwn 3 ponooroo BaTHOTO AMCKY BpaHI Ta bB1epi. Apeca pna 38epHeHs 8 Yipai: TO3 lopeanb Ypaira', ayn. MIpirvena, 4, KAia, C3680. Bupo6eMc NOpeanb Tlapwx,14,po PyaAnt 75008, lapux, opania. BupoonenO B Hwevwa. praTN npM Teweparypi +6C.+25C. TepuiH MpaTocni ta Howep napni HasegeH Ka ynaioe AaTa BwpoHALTE a36 icauis AO KIBepeoi ATM. BHICpHCTaH nporarow 6 uicauja nicna IDany opcnysaM. He BMIODHCTOBYSaTM nICTIR 30NE TEpMIHY 1pMJaTHOCTL M N LOREAL PARIS 14, rue Royale 75008 Paris FRANCE SA 75000 93584 ST OUEN CEDEXER www.loreal-paris.ru"""
+text5 = """aHOBne HonHH0 npeoopa3 OcnabneHHble n @HHO-TIapKne v eT noeBp nOMKMe Bon 6necTAWe. BOCCTAHOBNEHnE yCHIEHHAR AKTMBHbIM +TNAAKOCTh +B1O COPMA CHNEHH uEPAMHA ywacTKH MAbl rnyboko poHWKakOT B nOB 8onoC, 3anonHas MBaR KyTMKynpHblM Cno ne BGCTE akdnbTaeCA rnapkMMW W OnecTR pOHO OCTb, CTaHosHiCH napkuMMnl BOnocbl BHOBB 06peTa bl, npeiore 0BpexAeHWA, 00ecnewBaer p.TAHTEHO yTONjaeT oe yBnaxHeH n nTaHne. nPMMEHEHWE: HaHecnTe waMnyHA Te MaccWpylouyum pBWxeHRM CMO KonuecTBOM BOAbl. IIpn HeobxopVM0 XroMKOCTE Ha MOKpbie OOMTbHbIM K n08TOpWTe npoLeaypy. COCTAB/INGREDIENTS: BETAINE CYCERYL COcOATE,ODIOMC14-C1 SEC ALKYL Sug AURYL GLUCOsIDE LAUROYL PG-IRIMONIUM CHLORIDE CAPRYLICICAPRIC GLYCERIDES, TROECETH-6, TRIDECETH-12, PANHENOL, PARFUM (FRAGRANC GUAR HYDROXYPROPYLTRIMONIUM CHLORIDE, DIPROPYLENE GNOD PROPYLENE GLYCOL TRIDECETH-9, PEG-5 ETHYLHEXANCAE CERAMDE 3, PEG-120 METHYL GLUCOSE TRIOLEATE PROPAEDO DIUM EDTA, BENZYL ALCOHOL, METHYLCHLOROISOTHAZOUNOE VETHYLISOTHIAZOLINONE, CITRIC ACID (WATER) COCAMIDOPRP SULFATE, COCO-BETAINEP AQUA SODIUM LAURETH SILICONE QUATERNIJMAS paHMT npH TeMneparype He HMXe 5 'CW He Bbue 2 lo10n63083Tb no Ha3HayeHMIO nO MCTeyeHwA cpora ro aroroBWTEb: Cn «6ENVTA» 000 DEnapycb, 220089, r. MMHCK, yn. AekaópnCTOB, 29a Ten, +375(17) 222-91-17, www.belila.by PA3PASOTKA HAYYHOro LEHTPA c5E IMTA-BMISAG ERL 2017 400 M"""
+text6 = """DANGER FARE FARA VAARA KINAYNO ALU INGREDIENTS: BUTANE-ISOBUTANE-PROPA CYCLOPENTASILOXANE, ALUMINUM CHLOROHYOR 12-15 ALKYL BENZOATE, PARFUM/FRAGRAN DISTEARDIMONIUM HECTORITE, DIMETHU AEOALPINIA SPINOSA GUM, HYDROGEA POLYOECENE, SORBITAN SESQUIOLEATE, STEARATE, HERBACEUM (COTTON) POWDER, BHI LIMONENE, SILICA, GOSSY"""
+text7 = """GUS reAb-YxOAAAR Ayua KpeMN Kapau6OA" OT NIVEA TeAb AAR Ayua c MaCAOM MOHOH 3ab0THTCR O KOXe, ABAaR ee HesepoRTHO MATKON. HexHbH apoMaTTponwveckHX pyKTOB npofu Baww syBCTBa. NpespaTHTe exeAHeBHoe npHHATHE AYua B MOM HCTHHHOro HaCAaxAeHHR H 3a6OTH O cebe. lpHMeNeNHe: HakecwTe Aywa Ha Teno, noMacCHpyTe H CMONTe. MaberaRte nonaAaHMa Aasa. D/TransportRuntime.SQLiteEventStore(29467): Storing event with priority=VERY_LOW, name=FIREBASE_ML_SDK for destination cct AepMaTOAorwvecxH nporecTHposaNo. pH-c6anancHpoBaHO 6HopaaAaraeMan opMyna opMyAa 6es MMKDOnAaCTHKa 8 CooTBeTCTBNHConpeAeneHHeM NporpaMM He coAepxT MHKponAaCTHK 0OH no Oxpyxaxouei cpeAe. U reAAorARA AAR AYuy "Kpem I Kapam6oA" ala NIVEA. 38cTOcysaNHR: HBHecits reAb AMR AYIy Na tiAO, nOMacaxyire i sMwTe. YHMKaRTe norpanARHHR B ONÍ. He BHKODHCTOayaaTH B xapvoBHx yiARX pH s6aABNCOBaNO AepMaTOAOrivHO nporecToBano K2 NIVEA® yCINan "Kpew xone Kapaw6OAN" AYuKa apHaaraN KyriM real. KoNMBNny: AyuKa apHaAFaN reAhai ABHere xarbiNb3, ykaABNS MaNe xyi3. Kaare rycyine xOA ÓepMeNis. pH veNAecripiaren AepMaTOAOrwRAK CMNaKran oTkialiaren 0 NIVEA®-dan dush uchun "Krem va Karambol" gel-g'amxo'rilk. Foydalanish: dush uchun gelni badanga surting, uqalang va yuvib tashlang. Ko tushishidan saglaning, pH muvozanatiangan dermatologik sinovdan o't8 Cocraa/CxARA/Kupaw/Tarkibit: Aqua, Sodium www.NIVEA.com Laureth Sulfate, Cocamidopropyl Betaine, PEG-7 Glyceryl Cocoate, Parfum, Cocos Beiersdorf Nucifera Oil, Gardenia Taitensis Flower Beiersdorf AG, Extract, Glycerin, Sodium Chioride, PEG-3 Dstearate, Citric Acid, PEG-40 Hydrogenated -20245 Hamburg Castor Oil, PEG-200 Hydrogenated Glycervl arerOBETA/8ipyul/lshlah chi almate, Tocopherol, Sodium Benz0ate Bapobanc Belersdorf Manufactarlag Beri Franklestrasse 1, D-10587, Berlia, fepas, Germaniya/5aicpeanpe Sepals fiL, dpanuiTacoe 1, A10 Limonene, Linalool, Citronellol, Geraniol 250 MA/mle Cor Saiepcaspis, 105064, PuoEs, t. Mo Jexi Baa, &9, 5 , AEE 0-2000-753 Po"""
+text8 = """roREH O HOMEP FAPTWM 10.2020 (0 NGeave-On Rapid Growth and Thickness Hair Densilying M A MOgy1iH1OB8 He3MMBHM LeNaKw picr ifyeoa MD/RO Mezospray nespalabil Crestere rapda si Densilates MeoKO MIC Cnpewieybilz taiblh, Hbifbl3Aarslu waTH O TblFbl36FblHa KOMekrece MeooCnpew-ynnoTHMTenb HecMbl8aeMbiM «bblCrpbiM poc ycrora 8onoC)3000)ekiMBHOe cpepciB0, 11pepHa3Ne 08 A08 yeenMeHUS ryCTOTbl BOnoc BO BpeMa wx atMBHOO oetd. MezoxOKTennb akTMBHBIX KOMnOHeHTOB CnocoDet VeieMO BOnOCHAHBIX TyKOBML yBermveno 81pa8HMBaHHIO KyTMKYnbl BOnOC, yBa.RR ONBNMRO CTpyKrypbl BONOC io BCen anMHE. 0AT A8 BCOX TMnO8 BOTOG MMEHEHWE: HaHecHTe cpeneis0 CTble, 0acnpepe CRera eMnM cyxne Bonocbl.na ny4ulero pau y pacHeture BOnOcbl. He CMblB3 TABINGREDIENTS: AQUA(WATER A o12 PANTHEN LAURETHS FAT 0ALKV A 40 HYDROGENAlED R E ROPYLETHYLDIMON woan AWIs PE SODIUM HYALURONA A ETH PYI ENI PHENOXYE A S EA DE ABEN, PROPYLPAA AMAME NIACINAN AER, ARNICA M YLA INYL PAT AMINE HOL TN, PAN ACR US E, Dis SEFINYL ACETAIEB ASTA DtA FAEX ACI CA RRHIZATE INNG EINE G YLENE o 10A t DM0-2.N OLYQUAT OPONA, HEXYL CINN 3PAGOTKA HAY4HOrO PROPANE NALO0 NAMAL A-8MT3H AY Horo yEHTPA «5ET e B 261pE O ie incnonba0earb no Has WCTeHeM cpokarONHO WTA: 000 13rOToBHTENb: Cn b nap balse P6(17)22291 1 750 M bojlina"""
+text9 = """Dove 06bATMA HeHOCTH DDKpeMAna pyk Dove 06bATMA HeMHOCTH Macno wn npAHaA BaHMnb nopaayre ce6A KpeMoM AnA pyK Dove O6bATHA HEMHOCTH. OH OKyTaeT M3bICKaHHblM CoyeTaHneM nMTatebHoro KpeMa M Macha wM. Jlerko BnWTbIBaACb, OH nogapWT BaM He3abblBaeMoe oulyjeHne rnaAkoň n bapxaTMCTOW KOKW CHeXHbIMapoMaTOM npAHOM BaHMin. AepMaTonorwyeckw npOTeCTMpoBaH CogepxHT DeepCare Complex-KOMIneKc rnybokoro yXOna OT Dove cpoACTBeHHblMM KOXe yxaxMBaioulMMM KOMTOHeHTaMM M HaTypanbHbIM nMTaTenbHblM MaciOM. Tny6oko nWTaeT yBaXHAeT Bauy KOXY. ipMMeHeHMe: nerkwMM MaccnpyouMMW ABMKEHMAMM HaHeCHTe Ha Koxy ao nonHoro BnWTblBaHMA. C BOnpocaMM 06pauaMrecb no TeneooHy OT3bIB4MBO JIMHM 8-800-200-1-200.3B0HOK no PoccMM 6ecninaTHbl. apoMaT npAHO BaHWnn **poroBoñ cnOn anMAepMMca KpeM AnA pyk Dove 06iMMH HÍKHOCTI Macno WM Ta npama BaHinb AepMaTonoriuHo npoTeCTOBaH 3acTocyBaHHA:NerkMMM MacaxyioMMM pyxaMW HaHeciTb Ha IKipy Ao noBHOro nornWHaHHA. COCTAB/CKTIAD: Aqua, Glycerin, Stearic Acid, Caprylic/Capric Trigyoeride Dimethicone, Glycol Stearate, PEG-100 Stearate, Petrolatum, Acrylates/C103 Alkyl Acrylate Crosspolymer, Butyrospermum Parki (Shea) Buter, Carbone Cetyl Alcohol, Cyclopentasiloxane, Disodium EDTA, Glyceryl Stearate, Helianths Annuus (Sunfiower) Seed Oil, Methylparaben, Parfum, Phenoxyethan Propyparaben, Stearamide AMP, Triethanolamine, Coumarin, Cl 77891. HUBY VaroroBATenb: 000 «fOHwneBep Pycb, PoccHA, 123022, r. M y Lepren Maxeesa, A. 13. loCTaBK B Peciy6nMke Benapycb: 000 «3 . MWHCK, np-T lobepwTeRe 17-819, Ten. (+37517) 3453/ Yata naroTOBNeHMA, WcCnO BaTb Ao (HOMep napTH WBe Ty6bl. UA BHpoóHIHNC OHineBep PyCb, Poci, 12 M. MocKBa, Byn. CeprH Mauas byA. 13. Appeca nony poOHHLTBa: (Dinia TOB d Bep Pycb» B M. CRdl POcia, 620138, M. Exae Byn. KoMcOMONbCbKa Bp: TOB «{OHniBep 4119, YkpaiHa, M. K ApiBCbKa, byA 1en.: +38(044)490-30 rOTOB/NeHHR, Ipl Mep napTil): AMB. MOBH 36epiraHHR pi Bia +5C J0 75 ml (un) 2M c7o Caulb dove.com. www.unileve"""
+text10 = """52.50 52.50 57.70 52.50 724790- COCTAB/ CKAAA/ PAMbl: ISOBUTANEAGUA/ WATER DIMETHICONEALUMINUM CHLOROHYDRATE, PARFUM/FRAGRANCE, HYDROXYCRONELLAL, PHENOXYEIHANOL e Royale 75008 Panis FRANCE DIMETHIcONOL, LIMONENE, LINALOOL PERLITE,1ETRASODIUM GLUTAMATE DIACETATE 5000 93584STOUEN CEDEX FR LAURYL PEG-9POLYDIMETHYLSLORYETHYL DiIMETHICONE, HEXYLCINNAMAL (FIL CI61819/2) SARNIGR www.garnier.com"""
+text11 = """Best before BKopMCTaTH AO fopeH AO 4,59 INCI/Cknan/CoCTaB: Paraffinum Liquidum, Paraffin (and) Microcrystalline Wax, Petrolatum, Polybutene, Peg-40 Hydrogenated Castor Oil, Fragrance, Phenoxyethanol (and) Ethylherylglycerin, Tocopheryt Storage conditions/YMOBH 30epiraHHs/YcnoBMS XpaHeHHA +5-t2 ACTY 4774:2007 Acetate. Producer/BHpobHHKNS anchuka, 4. Astra CoS in ade in Ukraineoom"""
+
+main_text = text11 #задаем текст
+
+text_tuple = main_text.split(" ")
+
+text_tuple_marks = []
+
+for i in text_tuple:
+    if i.endswith(','):
+        text_tuple_marks.append(1)
+    elif i.endswith(':'):
+        text_tuple_marks.append(2)
+    elif i.endswith('.'):
+        text_tuple_marks.append(3)
+    else:
+        text_tuple_marks.append(0)
+
+for i in range(len(text_tuple_marks)):
+    if text_tuple[i].endswith(':') == False and text_tuple[i].isupper():
+        text_tuple_marks[i] = 1
+
+varuety_tuples = []
+final_tuple = []
+count_zero = 0
+for i in range(len(text_tuple_marks)):
+    el = text_tuple_marks[i]
+    if el == 0:
+        count_zero += 1
+    if el == 1:
+        count_zero = 0
+    
+    final_tuple.append(text_tuple[i])
+
+    if el == 2 and len(final_tuple) == count_zero:
+        final_tuple = []
+        count_zero = 0
+    
+    if el == 2 and len(final_tuple) > count_zero:
+        varuety_tuples.append(final_tuple)
+        final_tuple = []
+        count_zero = 0
+
+    if count_zero == 6 and len(final_tuple) > count_zero:
+        varuety_tuples.append(final_tuple)
+        final_tuple = []
+        count_zero = 0
+
+    if count_zero == 6 and len(final_tuple) == count_zero:
+        final_tuple = []
+        count_zero = 0
+
+    if el == 3 and len(final_tuple) == count_zero:
+        final_tuple = []
+        count_zero = 0
+    
+    if el == 3 and len(final_tuple) > count_zero:
+        varuety_tuples.append(final_tuple)
+        final_tuple = []
+        count_zero = 0
+    
+varuety_tuples.append(final_tuple)
+
+print(text_tuple_marks)
+print(varuety_tuples)
+print(max_tuple(varuety_tuples))
