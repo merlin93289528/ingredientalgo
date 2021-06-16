580cf6416daffa7fe2d0c0dc405724a84e4bfcfe
merlin7 <40037286+merlin93289528@users.noreply.github.com>
2021-06-16 18:58:04 +0300

    V1.8

--- main.dart
+++ main.dart
@@ -1,5 +1,49 @@
 //------------Constants-------------------
-const main_text = """aHOBne HonHH0 npeoopa3 OcnabneHHble n @HHO-TIapKne v eT noeBp nOMKMe Bon 6necTAWe. BOCCTAHOBNEHnE yCHIEHHAR AKTMBHbIM +TNAAKOCTh +B1O COPMA CHNEHH uEPAMHA ywacTKH MAbl rnyboko poHWKakOT B nOB 8onoC, 3anonHas MBaR KyTMKynpHblM Cno ne BGCTE akdnbTaeCA rnapkMMW W OnecTR pOHO OCTb, CTaHosHiCH napkuMMnl BOnocbl BHOBB 06peTa bl, npeiore 0BpexAeHWA, 00ecnewBaer p.TAHTEHO yTONjaeT oe yBnaxHeH n nTaHne. nPMMEHEHWE: HaHecnTe waMnyHA Te MaccWpylouyum pBWxeHRM CMO KonuecTBOM BOAbl. IIpn HeobxopVM0 XroMKOCTE Ha MOKpbie OOMTbHbIM K n08TOpWTe npoLeaypy. COCTAB/INGREDIENTS: BETAINE CYCERYL COcOATE,ODIOMC14-C1 SEC ALKYL Sug AURYL GLUCOsIDE LAUROYL PG-IRIMONIUM CHLORIDE CAPRYLICICAPRIC GLYCERIDES, TROECETH-6, TRIDECETH-12, PANHENOL, PARFUM (FRAGRANC GUAR HYDROXYPROPYLTRIMONIUM CHLORIDE, DIPROPYLENE GNOD PROPYLENE GLYCOL TRIDECETH-9, PEG-5 ETHYLHEXANCAE CERAMDE 3, PEG-120 METHYL GLUCOSE TRIOLEATE PROPAEDO DIUM EDTA, BENZYL ALCOHOL, METHYLCHLOROISOTHAZOUNOE VETHYLISOTHIAZOLINONE, CITRIC ACID (WATER) COCAMIDOPRP SULFATE, COCO-BETAINEP AQUA SODIUM LAURETH SILICONE QUATERNIJMAS paHMT npH TeMneparype He HMXe 5 'CW He Bbue 2 lo10n63083Tb no Ha3HayeHMIO nO MCTeyeHwA cpora ro aroroBWTEb: Cn «6ENVTA» 000 DEnapycb, 220089, r. MMHCK, yn. AekaópnCTOB, 29a Ten, +375(17) 222-91-17, www.belila.by PA3PASOTKA HAYYHOro LEHTPA c5E IMTA-BMISAG ERL 2017 400 M""";
+const main_text = """NPMMEHEHME
+YrpoM H BeMepoM,
+CHOVHB BATHBIM
+AMCK.
+HaHecHTe Ha nHYO
+MOBTOpMTe po Tex
+M wero. He TpeőyeT TpeHAA nop noka BaTHBIM
+AMCK He CraHer
+MneanbHo 4HCTBIM.
+M CMbIBaHMA.
+6E3 CNMPTA. BE3 OTAYLWEK
+ipOTecTwpoBAHO NOA AEpMaToNornyecKUM M OdDTONBManormyeckUM
+KOHTpOneM.
+LEHHOCTH L'OREAL PARIS
+Be3onacHocTb M 3hheKTMBHOCTb AOKa3aHa He3aBW
+CMMBIMM MHCTHTyTaMM npM MMHMMyM 600 HaHeceHMAX Ha
+KOXy, BKNKO4aA uyBCTBMTeibHylo.
+LOréal npwaepxeH MeTOgaM TeCTHPOBaHHA 6e3
+KMBOTHBIX 6onee 30 neT.
+781988 1-COCTABICKNAD: AQUA /WATER HEXYLENE GLYCOL
+GLYCERIN .POLOXAMER 184 DISODIUM COCOAMPHODIACETAIE
+DISODIUM EDTA POLYAMINOPROPYL BIGUANIDE. (FIL.L. B548942
+FOEAU IOpeare), 119180, . Mooxsa, 4HTanyreHwHcoAÁ nep, 18, Cīp.1-2. Ten: (454913
+EHE. TEaTb Iporyayion poinuaNg| TMBX 34p Ko. KT Hertzstrasse 175 DI6187 KARLSRURE,
+GEmay epk3ARL IQgeH pO: CHL H2 ynaoere. TenepOH TopAe HEHM NOpeanb 1lapex8 800 10 9 0. b
+CTHpTa-He CopepoAT 3THTIOBAĂ CTHDT. "YnonHoMOHEeHHBN nOepCraBHTens k3roT08MTenol HMNODTeP.
+28T602 06/23
+3MHBaHR BOONO. 33cnoxowe Wipy. HawociTH 8a
+Wpy Donwn 3 ponooroo BaTHOTO AMCKY BpaHI
+Ta bB1epi. Apeca pna 38epHeHs 8 Yipai: TO3
+lopeanb Ypaira', ayn. MIpirvena, 4, KAia,
+C3680. Bupo6eMc NOpeanb Tlapwx,14,po PyaAnt
+75008, lapux, opania. BupoonenO B Hwevwa.
+praTN npM Teweparypi +6C.+25C. TepuiH
+MpaTocni ta Howep napni HasegeH Ka ynaioe
+AaTa BwpoHALTE a36 icauis AO KIBepeoi
+ATM. BHICpHCTaH nporarow 6 uicauja nicna
+IDany opcnysaM. He BMIODHCTOBYSaTM nICTIR
+30NE TEpMIHY 1pMJaTHOCTL
+M
+N
+LOREAL PARIS
+14, rue Royale 75008 Paris FRANCE
+SA 75000 93584 ST OUEN CEDEXER
+www.loreal-paris.ru""";
 
 const allCapitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ./,&:;";
 var allCapitalsTuple = allCapitals.split('');
@@ -40,8 +84,8 @@ List formatTuple(List tuple) {
   for (var el in tuple) {
  
     if (el.endsWith(",") || el.endsWith(".")) {
-      ingredient += " " + el;
-      result.add(ingredient.replaceAll(RegExp(r'\,|\.'), ''));
+      ingredient += el;
+      result.add(ingredient.replaceAll(RegExp(r'\,|\.'), '').replaceAll(RegExp(r'\n'), ' '));
       ingredient = "";
       continue;
     }
@@ -50,16 +94,16 @@ List formatTuple(List tuple) {
     
     if (is_contains_coma || el.contains(".")) {
       var el_tuple = is_contains_coma? el.split(",") : el.split(".");
-      ingredient += " " + el_tuple[0];
-      result.add(ingredient);
+      ingredient += el_tuple[0];
+      result.add(ingredient.replaceAll(RegExp(r'\,|\.'), '').replaceAll(RegExp(r'\n'), ' '));
       ingredient = el_tuple[1];
       continue;
     }
     
-    ingredient += " " + el;
+    ingredient += el + " ";
   }
   
-  result.add(ingredient.replaceAll(RegExp(r'\,|\.'), ''));
+  result.add(ingredient.replaceAll(RegExp(r'\,|\.'), '').replaceAll(RegExp(r'\n'), ' '));
   
   return result;
 }
